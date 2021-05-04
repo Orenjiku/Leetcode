3cba57b246ac26f88fe67d5d501565620e3ce13b
William Chang <wdchang86@gmail.com>
2021-05-04 18:31:06 -0400

    Time: 120 ms (24.48%), Space: 49.9 MB (44.48%) - LeetHub

--- minimum-deletion-cost-to-avoid-repeating-letters/minimum-deletion-cost-to-avoid-repeating-letters.js
+++ minimum-deletion-cost-to-avoid-repeating-letters/minimum-deletion-cost-to-avoid-repeating-letters.js
@@ -5,19 +5,15 @@
  */
 var minCost = function(s, cost) {
     let result = 0;
-    let maximum = cost[0];
-    let total = cost[0];
-    let count = 1;
+    let max = 0;
+    let total = 0;
     for (let i = 0; i < s.length; i++) {
-        if (s[i] === s[i + 1]) {
-            maximum = Math.max(maximum, cost[i + 1]);
-            total += cost[i + 1];
-            count++;
-        } else {
-            count > 1 ? result += total - maximum : result += 0;
-            maximum = cost[i + 1];
-            total = cost[i + 1];
-            count = 1;
+        max = Math.max(max, cost[i]);
+        total += cost[i];
+        if (s[i] !== s[i + 1]) {
+            result += total - max;
+            max = 0;
+            total = 0;
         }
     }
     return result;
