93ed12009950a752b4833bba06440924cc767b1b
William Chang <wdchang86@gmail.com>
2021-05-12 14:55:34 -0400

    Time: 80 ms (72.31%), Space: 40.7 MB (15.38%) - LeetHub

--- reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.js
+++ reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.js
@@ -5,7 +5,6 @@
 var reverseParentheses = function(s) {
     while (/\([a-z]*\)/g.test(s)) {
         const words = s.match(/\([a-z]*\)/g);
-        console.log(s, words)
         words.forEach(word => {
             const reversedWord = word.slice(1, word.length - 1).split('').reverse().join('');
             s = s.replace(word, reversedWord);
