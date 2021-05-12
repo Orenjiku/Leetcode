fa09779c2f36eb1034223a46c1cc5f1ce319b65c
William Chang <wdchang86@gmail.com>
2021-05-12 14:50:31 -0400

    Time: 88 ms (18.46%), Space: 40 MB (50.77%) - LeetHub

---
+++ reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseParentheses = function(s) {
+    while (/\([a-z]*\)/g.test(s)) {
+        const words = s.match(/\([a-z]*\)/g);
+        console.log(s, words)
+        words.forEach(word => {
+            const reversedWord = word.slice(1, word.length - 1).split('').reverse().join('');
+            s = s.replace(word, reversedWord);
+        })
+    }
+    return s;
+};
