6aa0e2dd54cf36a1d35ea973f7dd9f78170417ea
William Chang <wdchang86@gmail.com>
2021-05-13 15:28:54 -0400

    Time: 96 ms (47.97%), Space: 41.1 MB (62.92%) - LeetHub

--- insert-interval/insert-interval.js
+++ insert-interval/insert-interval.js
@@ -9,21 +9,20 @@ var insert = function(intervals, newInterval) {
     let insertStart = false;
     let start = 0;
     let end = 0;
-    let min = newInterval[0];
-    let max = newInterval[1];
+    const combinedInterval = [...newInterval];
     for (let i = 0; i < intervals.length; i++) {
         if (newInterval[0] <= intervals[i][1] && insertStart === false) {
             insertStart = true;
             start = i;
-            min = Math.min(newInterval[0], intervals[i][0]);
+            combinedInterval[0] = Math.min(newInterval[0], intervals[i][0]);
         }
         if (newInterval[1] >= intervals[i][0]) {
             end = i;
-            max = Math.max(newInterval[1], intervals[i][1]);
+            combinedInterval[1] = Math.max(newInterval[1], intervals[i][1]);
         }
     }
     if (newInterval[1] < intervals[0][0]) intervals.unshift(newInterval);
     else if (newInterval[0] > intervals[intervals.length - 1][1]) intervals.push(newInterval);
-    else intervals.splice(start, end - start + 1, [min, max]);
+    else intervals.splice(start, end - start + 1, combinedInterval);
     return intervals;
 };
