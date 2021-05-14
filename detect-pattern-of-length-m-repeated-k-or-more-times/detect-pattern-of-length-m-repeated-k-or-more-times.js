256d3cee284421fa358bc17a31ec251cfd824b2e
William Chang <wdchang86@gmail.com>
2021-05-14 19:01:47 -0400

    Time: 84 ms (29.09%), Space: 40.4 MB (34.55%) - LeetHub

--- detect-pattern-of-length-m-repeated-k-or-more-times/detect-pattern-of-length-m-repeated-k-or-more-times.js
+++ detect-pattern-of-length-m-repeated-k-or-more-times/detect-pattern-of-length-m-repeated-k-or-more-times.js
@@ -5,18 +5,17 @@
  * @return {boolean}
  */
 var containsPattern = function(arr, m, k) {
-    const map = new Map();
     for (let i = 0; i < arr.length; i++) {
         const pattern = arr.slice(i, i + m).join('');
         let count = 1;
-        let flag = true;
         let j = i + m;
-        while (flag === true) {
+        while (true) {
             const nextPattern = arr.slice(j, j + m).join('');
-            pattern === nextPattern ? count++ : flag = false;
+            if (pattern === nextPattern) count++;
+            else break;
+            if (count === k) return true;
             j += m;
         }
-        if (count === k) return true;
     }
     return false;
 };
