97361ca2759717b48ab1d99e9ea75590816ca062
William Chang <wdchang86@gmail.com>
2021-05-15 18:45:01 -0400

    Time: 84 ms (70.46%), Space: 39.9 MB (38.58%) - LeetHub

--- longest-common-prefix/longest-common-prefix.js
+++ longest-common-prefix/longest-common-prefix.js
@@ -5,8 +5,9 @@
 var longestCommonPrefix = function(strs) {
     if (strs.length === 0) return '';
     for (let i = 1; i <= strs[0].length; i++) {
+        const pattern = strs[0].slice(0, i)
         for (let j = 1; j < strs.length; j++) {
-            if (strs[0].slice(0, i) !== strs[j].slice(0, i)) return strs[j].slice(0, i - 1)
+            if (pattern !== strs[j].slice(0, i)) return strs[j].slice(0, i - 1)
         }
     }
     return strs[0];
