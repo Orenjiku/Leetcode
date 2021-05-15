90d4866b846ba31f66a7153d7289249b79db66a0
William Chang <wdchang86@gmail.com>
2021-05-14 22:50:00 -0400

    Time: 88 ms (19.44%), Space: 38.5 MB (90.36%) - LeetHub

---
+++ maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function(s) {
+    let maxCount = 0;
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') count++;
+        maxCount = Math.max(maxCount, count);
+        if (s[i] === ')') count--;
+    }
+    return maxCount;
+};
