9a72b9357c110cc95ba6ac8f3ee892077f8475d9
William Chang <wdchang86@gmail.com>
2021-05-14 22:35:18 -0400

    Time: 80 ms (63.23%), Space: 38.4 MB (90.97%) - LeetHub

--- maximum-repeating-substring/maximum-repeating-substring.js
+++ maximum-repeating-substring/maximum-repeating-substring.js
@@ -4,17 +4,11 @@
  * @return {number}
  */
 var maxRepeating = function(sequence, word) {
-    let i = 0;
-    const wordLen = word.length
-    let maxCount = 0;
-    for (let i = 0; i < sequence.length; i++) {
-        let j = i;
-        let count = 0;
-        while (sequence.slice(j, j + wordLen) === word) {
-            count++;
-            j = j + wordLen;
-        }
-        maxCount = Math.max(maxCount, count);
+    let count = 0;
+	let pattern = word;
+    while(sequence.includes(pattern)) {
+        pattern += word;
+      	count++
     }
-    return maxCount;
+    return count
 };
