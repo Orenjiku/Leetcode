5cff3b61ec808f1c55eba6b7a0a7bdb4c9bad4df
William Chang <wdchang86@gmail.com>
2021-05-15 13:08:46 -0400

    Time: 80 ms (64.83%), Space: 38.8 MB (30.71%) - LeetHub

---
+++ determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.js
@@ -0,0 +1,7 @@
+/**
+ * @param {string} coordinates
+ * @return {boolean}
+ */
+var squareIsWhite = function(coordinates) {
+    return coordinates[0].charCodeAt() % 2 === coordinates[1] % 2 ? false : true;
+};
