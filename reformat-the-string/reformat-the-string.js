8144560cd09cd7b3161e74d9154c7dcf759bf39a
William Chang <wdchang86@gmail.com>
2021-05-16 15:22:55 -0400

    Time: 92 ms (76.74%), Space: 42.9 MB (61.24%) - LeetHub

--- reformat-the-string/reformat-the-string.js
+++ reformat-the-string/reformat-the-string.js
@@ -5,15 +5,14 @@
 var reformat = function(s) {
     const lets = s.match(/[a-z]/g) || [];
     const nums = s.match(/\d/g) || [];
-    if (Math.abs(lets.length - nums.length) > 1) return '';
-    
-    const reformatStr = (a, b, str) => {
-        if (b.length > a.length) return reformatStr(b, a, '');
-        while (a.length > 0) {
-            let charB = b.length > 0 ? b.pop() : '';
-            str += a.pop() + charB;
-        }
-        return str;
-    }
-    return reformatStr(lets, nums, '');
+    return (Math.abs(lets.length - nums.length) > 1) ? '' : reformatStr(lets, nums, '');
 };
+
+function reformatStr(a, b, str) {
+    if (b.length > a.length) return reformatStr(b, a, '');
+    while (a.length > 0) {
+        let charB = b.length > 0 ? b.pop() : '';
+        str += a.pop() + charB;
+    }
+    return str;
+}
