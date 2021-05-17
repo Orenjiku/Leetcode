fbf448aa145f157417a4f2e2eb943180a16fc265
William Chang <wdchang86@gmail.com>
2021-05-17 01:12:28 -0400

    Time: 100 ms (23.30%), Space: 40.1 MB (91.76%) - LeetHub

--- toeplitz-matrix/toeplitz-matrix.js
+++ toeplitz-matrix/toeplitz-matrix.js
@@ -3,21 +3,18 @@
  * @return {boolean}
  */
 var isToeplitzMatrix = function(matrix) {
-    let col = 0 - matrix.length - 1;
-    while (col < matrix[0].length) {
-        const nums = new Set();
+    let start = 0 - matrix.length + 1;
+    while (start < matrix[0].length) {
+        const nums = new Set().add(undefined);
         let row = 0;
-        let curCol = col;
+        let col = start;
         while (row < matrix.length) {
-            if (matrix[row][curCol] !== undefined && nums.size === 0) {
-                nums.add(matrix[row][curCol]);
-            } else if (matrix[row][curCol] !== undefined && !nums.has(matrix[row][curCol]) && nums.size === 1) {
-                return false;
-            }
+            nums.add(matrix[row][col]);
+            if (nums.size > 2) return false;
             row++;
-            curCol++;
+            col++;
         }
-        col++;
+        start++;
     }
     return true;
 };
