73ab8324337f7b4c0d5f0dfaf6e4bcb872720b86
William Chang <wdchang86@gmail.com>
2021-05-21 13:37:53 -0400

    Time: 100 ms (79.45%), Space: 45.1 MB (67.81%) - LeetHub

--- number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.js
+++ number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.js
@@ -5,13 +5,10 @@
 var numEquivDominoPairs = function(dominoes) {
     const map = {};
     for (let i = 0; i < dominoes.length; i++) {
-        const dominoStr = `${dominoes[i][0]}${dominoes[i][1]}`;
-        const complementStr = `${dominoes[i][1]}${dominoes[i][0]}`;
-        if (!map[dominoStr] && !map[complementStr]) {
-            map[dominoStr] = [0, 0];
-        }
-        map[dominoStr] ? map[dominoStr][0]++ : map[complementStr][0]++;
-        map[dominoStr] ? map[dominoStr][1] = map[dominoStr][0] - 1 + map[dominoStr][1] : map[complementStr][1] = map[complementStr][0] - 1 + map[complementStr][1];
+        const dominoStr = `${Math.min(...dominoes[i])}${Math.max(...dominoes[i])}`;
+        if (!map[dominoStr]) map[dominoStr] = [0, 0];
+        map[dominoStr][0]++
+        map[dominoStr][1] = map[dominoStr][0] - 1 + map[dominoStr][1];
     }
     return Object.values(map).reduce((acc, cur) => acc += cur[1], 0);
 };
