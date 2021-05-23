a76c64cdf1c1b75a1582f01f785ae5fde28809fb
William Chang <wdchang86@gmail.com>
2021-05-22 22:25:08 -0400

    Time: 244 ms (18.75%), Space: 87.7 MB (6.25%) - LeetHub

---
+++ longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var longestZigZag = function(root) {
+    let maxCount = 0;
+    const traverse = (node) => {
+        if (!node) return [0, 0, 0];
+        let left = traverse(node.left);
+        let right = traverse(node.right);
+        let leftCount = node.left ? left[1] + 1 : 0;
+        let rightCount = node.right ? right[0] + 1 : 0;
+        maxCount = Math.max(leftCount, rightCount, maxCount);
+        return [leftCount, rightCount, maxCount];
+    }
+    return traverse(root)[2];
+};
