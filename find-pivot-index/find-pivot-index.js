5dd224c2b6165dfce33d9d6b741348cea5a1bbc5
William Chang <wdchang86@gmail.com>
2021-05-24 09:35:05 -0400

    Time: 104 ms (43.51%), Space: 40.8 MB (52.49%) - LeetHub

--- find-pivot-index/find-pivot-index.js
+++ find-pivot-index/find-pivot-index.js
@@ -4,23 +4,12 @@
  */
 var pivotIndex = function(nums) {
     let total = 0;
-    const leftSum = nums.reduce((acc, cur, i) => {
-        total += cur;
-        acc[i - 1] === undefined ? acc.push(cur) : acc.push(cur + acc[acc.length - 1]);
-        return acc;
-    }, []);
-    const rightSum = nums.reduce((acc, cur, i) => {
-        if (i === 0) {
-            acc.push(total);
-        } else {
-            acc.push(total - nums[i - 1]);
-            total -= nums[i - 1];
-        }
-        return acc;
-    }, []);
-    
-    for (let i = 0; i < leftSum.length; i++) {
-        if (leftSum[i] === rightSum[i]) return i;
+    let leftSum = 0;
+    nums.forEach(num => total += num);
+    for (let i = 0; i < nums.length; i++) {
+        total -= nums[i]
+        if (leftSum === total) return i;
+        leftSum += nums[i];
     }
     return -1;
 };
