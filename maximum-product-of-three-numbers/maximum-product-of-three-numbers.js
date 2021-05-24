e30f30a1dff67db3e84e4457bd176e63331f1a62
William Chang <wdchang86@gmail.com>
2021-05-23 21:35:03 -0400

    Time: 124 ms (52.24%), Space: 43 MB (36.42%) - LeetHub

---
+++ maximum-product-of-three-numbers/maximum-product-of-three-numbers.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+    nums.sort((a, b) => a - b);
+    if (nums[0] > 0 || nums[nums.length - 1] < 0) return nums[nums.length - 3] * nums[nums.length - 2] * nums[nums.length - 1];
+    else return Math.max(nums[0] * nums[1] * nums[nums.length - 1], nums[nums.length - 3] * nums[nums.length - 2] * nums[nums.length - 1])
+};
