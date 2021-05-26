df051f28ff1b47ab8df10731bde3083b96e79df8
William Chang <wdchang86@gmail.com>
2021-05-26 19:09:39 -0400

    Time: 72 ms (88.28%), Space: 38.3 MB (56.78%) - LeetHub

--- excel-sheet-column-title/excel-sheet-column-title.js
+++ excel-sheet-column-title/excel-sheet-column-title.js
@@ -3,11 +3,9 @@
  * @return {string}
  */
 var convertToTitle = function(columnNumber) {
-    const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     let result = '';
     while (columnNumber > 0) {
-        const idx = (columnNumber - 1) % 26;
-        result = letters[idx] + result;
+        result = String.fromCharCode((columnNumber - 1) % 26 + 65) + result;
         columnNumber = columnNumber % 26 === 0 ? Math.floor(columnNumber / 26) - 1 : Math.floor(columnNumber / 26);
     }
     return result;
