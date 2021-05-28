3d67d8064af139603b8bdfd214dc54946ce9f698
William Chang <wdchang86@gmail.com>
2021-05-28 09:41:19 -0400

    Time: 76 ms (96.36%), Space: 38.5 MB (78.81%) - LeetHub

--- perfect-number/perfect-number.js
+++ perfect-number/perfect-number.js
@@ -3,10 +3,9 @@
  * @return {boolean}
  */
 var checkPerfectNumber = function(num) {
-    if (num === 1) return false;
-    sum = 1;
-    for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++) {
-         if (num % i === 0) sum += i + num / i;
+    sum = 0;
+    for (let i = 1; i <= Math.floor(Math.sqrt(num)); i++) {
+        if (num % i === 0) sum += i + num / i;
     }
-    return sum === num;
+    return sum - num !== 1 && sum - num === num;
 };
