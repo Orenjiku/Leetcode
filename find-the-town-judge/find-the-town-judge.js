afe9465eab929516620a150f2d6020de28190169
William Chang <wdchang86@gmail.com>
2021-05-30 21:11:26 -0400

    Time: 112 ms (80.31%), Space: 47 MB (32.55%) - LeetHub

--- find-the-town-judge/find-the-town-judge.js
+++ find-the-town-judge/find-the-town-judge.js
@@ -4,13 +4,10 @@
  * @return {number}
  */
 var findJudge = function(n, trust) {
-    if (n === 1 && trust.length === 0) return 1;
-    const map = {};
+    const arr = Array.from(new Array(n), () => new Set());
     for (let i = 0; i < trust.length; i++) {
-        if (!map.hasOwnProperty(trust[i][0])) map[trust[i][0]] = [new Set(), new Set()];    
-        if (!map.hasOwnProperty(trust[i][1])) map[trust[i][1]] = [new Set(), new Set()];
-        map[trust[i][0]][1].add(trust[i][1]);
-        map[trust[i][1]][0].add(trust[i][0]);
+        if (arr[trust[i][1] - 1]) arr[trust[i][1] - 1].add(trust[i][0]);
+        arr[trust[i][0] - 1] = false;
     }
-    return Object.keys(map).reduce((acc, cur) => map[cur][0].size === n - 1 && map[cur][1].size === 0 ? cur : acc, -1)
+    return arr.reduce((acc, cur, i) => cur && cur.size === n - 1 ? i + 1 : acc, -1)
 };
