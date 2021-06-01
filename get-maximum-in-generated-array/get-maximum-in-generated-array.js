3cf45ddddd25ef4de1e4260196b01be5223c9a18
William Chang <wdchang86@gmail.com>
2021-06-01 13:34:59 -0400

    Time: 88 ms (19.72%), Space: 38.6 MB (70.42%) - LeetHub

---
+++ get-maximum-in-generated-array/get-maximum-in-generated-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var getMaximumGenerated = function(n) {
+    const nums = new Array(n + 1).fill(0);
+    if (n + 1 > 1) nums[1] = 1;
+    for (let i = 1; i <= n; i++) {
+        if (2 * i >= 2 && 2 * i <= n) nums[2 * i] = nums[i];
+        if (2 * i + 1 >= 2 && 2 * i + 1 <= n) nums[2 * i + 1] = nums[i] + nums[i + 1];
+    }
+    return Math.max(...nums);
+};
