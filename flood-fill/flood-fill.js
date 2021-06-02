020aad605ce94f0497b95ffea924f649bc6607a5
William Chang <wdchang86@gmail.com>
2021-06-02 11:55:15 -0400

    Time: 148 ms (5.09%), Space: 47 MB (5.70%) - LeetHub

---
+++ flood-fill/flood-fill.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} newColor
+ * @return {number[][]}
+ */
+var floodFill = function(image, sr, sc, newColor) {
+    const traverse = (row, col, image, set, color, newColor) => {
+        if (row < 0 || row >= image.length || col < 0 || col >= image[0].length || set.has(`${row}${col}`) || image[row][col] !== color) return;
+        image[row][col] = newColor;
+        set.add(`${row}${col}`);
+        traverse(row + 1, col, image, set, color, newColor);
+        traverse(row, col + 1, image, set, color, newColor);
+        traverse(row - 1, col, image, set, color, newColor);
+        traverse(row, col - 1, image, set, color, newColor);
+        
+    }
+    traverse(sr, sc, image, new Set(), image[sr][sc], newColor);
+    console.log(image)
+    return image;
+};
