c0b2a7d9b035160881ce4756527ee56dfecc5dcc
William Chang <wdchang86@gmail.com>
2021-06-02 15:24:46 -0400

    Time: 124 ms (6.90%), Space: 40.6 MB (48.28%) - LeetHub

--- most-visited-sector-in-a-circular-track/most-visited-sector-in-a-circular-track.js
+++ most-visited-sector-in-a-circular-track/most-visited-sector-in-a-circular-track.js
@@ -5,7 +5,6 @@
  */
 var mostVisited = function(n, rounds) {
     const arr = new Array(n).fill(0);
-    let max = 1;
     let j = rounds[0] - 1;
     arr[j] = 1;
     for (let i = 1; i < rounds.length; i++) {
@@ -13,10 +12,10 @@ var mostVisited = function(n, rounds) {
         while (count > 0) {
             j = (j + 1) % n;
             arr[j] += 1;
-            max = Math.max(max, arr[j]);
             count--;
         }
     }
+    const max = Math.max(...arr);
     return arr.reduce((acc, cur, i) => {
         if (cur === max) acc.push(i + 1);
         return acc;
