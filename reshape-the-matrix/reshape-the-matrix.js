c4ea1f5250b5b5b48e6acc0ab0dec11e0bff273f
William Chang <wdchang86@gmail.com>
2021-06-08 19:14:12 -0400

    Time: 112 ms (15.76%), Space: 42.1 MB (74.55%) - LeetHub

--- reshape-the-matrix/reshape-the-matrix.js
+++ reshape-the-matrix/reshape-the-matrix.js
@@ -5,19 +5,17 @@
  * @return {number[][]}
  */
 var matrixReshape = function(mat, r, c) {
-    if (mat.length * mat[0].length > r * c) return mat;
+    if (mat.length * mat[0].length !== r * c) return mat;
     const newMatrix = Array.from(new Array(r), () => new Array(c).fill(0));
     let m = 0;
     let n = 0;
     for (let i = 0; i < newMatrix.length; i++) {
         for (let j = 0; j < newMatrix[i].length; j++) {
             newMatrix[i][j] = mat[m][n];
-            if (n + 1 <= mat[0].length - 1) {
-                n++;
-            } else {
+            n++;
+            if (n === mat[0].length) {
                 n = 0;
                 m++;
-                if (m === mat.length && (i < newMatrix.length - 1 || j < newMatrix[i].length - 1)) return mat;
             }
         }
     }
