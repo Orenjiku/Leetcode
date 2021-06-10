29f8bedb78bd9916cf24938abce1333bed386e16
William Chang <wdchang86@gmail.com>
2021-06-10 09:05:27 -0400

    Time: 84 ms (64.40%), Space: 37.8 MB (99.60%) - LeetHub

--- string-matching-in-an-array/string-matching-in-an-array.js
+++ string-matching-in-an-array/string-matching-in-an-array.js
@@ -6,7 +6,7 @@ var stringMatching = function(words) {
     const result = new Set();
     for (let i = 0; i < words.length; i++) {
         for (let j = 0; j < words.length; j++) {
-            if (i !== j && new RegExp(`${words[j]}`).test(words[i])) result.add(words[j]);
+            if (i !== j && words[i].includes(words[j])) result.add(words[j]);
         }
     }
     return Array.from(result);
