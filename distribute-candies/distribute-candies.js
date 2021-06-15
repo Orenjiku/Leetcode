602d32051bcc0194ef36bed44c5e144f121c917a
William Chang <wdchang86@gmail.com>
2021-06-15 14:54:34 -0400

    Time: 132 ms (84.04%), Space: 52.2 MB (64.08%) - LeetHub

--- distribute-candies/distribute-candies.js
+++ distribute-candies/distribute-candies.js
@@ -3,10 +3,6 @@
  * @return {number}
  */
 var distributeCandies = function(candyType) {
-    const map = candyType.reduce((acc, cur) => {
-        acc[cur] = (acc[cur] || 0) + 1;
-        return acc;
-    }, {})
-    
-    return Math.min(candyType.length / 2, Object.keys(map).length);
+    const set = new Set([...candyType])
+    return Math.min(candyType.length / 2, set.size);
 };
