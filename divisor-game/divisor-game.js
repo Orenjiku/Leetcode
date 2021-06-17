85260a7ee33c8981a431119eb57faf58cd99786a
William Chang <wdchang86@gmail.com>
2021-06-17 10:47:37 -0400

    Time: 72 ms (93.59%), Space: 38.7 MB (13.54%) - LeetHub

--- divisor-game/divisor-game.js
+++ divisor-game/divisor-game.js
@@ -3,5 +3,15 @@
  * @return {boolean}
  */
 var divisorGame = function(n) {
-    return n % 2 === 0
+    let turn = false;
+    while (n > 1) {
+        for (let i = 1; i < n; i++) {
+            if (n % i === 0)  {
+                n -= i; 
+                turn = !turn;
+                break;
+            }
+        }
+    }
+    return turn % 2 === 1 ? true : false;
 };
