8950a4701bac1b4cdab7a6c91fa43bf6c022c7f7
William Chang <wdchang86@gmail.com>
2021-06-16 21:01:59 -0400

    Time: 84 ms (91.35%), Space: 39.6 MB (69.23%) - LeetHub

--- count-largest-group/count-largest-group.js
+++ count-largest-group/count-largest-group.js
@@ -3,17 +3,12 @@
  * @return {number}
  */
 var countLargestGroup = function(n) {
-    const map = {};
-    let max = 0;
+    const arr = new Array(37).fill(0);
     for (let i = n; i >= 1; i--) {
-        if (i > 9) {
-            const sum = i.toString().split('').reduce((acc, cur) => acc + Number(cur), 0);
-            map[sum] = (map[sum] || 0) + 1;
-            max = Math.max(max, map[sum]);
-        } else {
-            map[i] = (map[i] || 0) + 1;
-            max = Math.max(max, map[i]);
-        }
+        arr[getSum(i)] += 1;
     }
-    return Object.values(map).filter(num => num === max).length;
+    const max = Math.max(...arr);
+    return arr.filter(num => num === max).length;
 };
+
+const getSum = (num) => num === 0 ? 0 : num % 10 + getSum(Math.floor(num / 10));
