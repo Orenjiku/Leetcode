d0742c6cd2b676258de4d7783b5999743a43ab81
William Chang <wdchang86@gmail.com>
2021-06-18 13:41:00 -0400

    Time: 100 ms (68.89%), Space: 46.7 MB (30.59%) - LeetHub

--- partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.js
+++ partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.js
@@ -3,11 +3,5 @@
  * @return {number}
  */
 var minPartitions = function(n) {
-    let count = 0;
-    const finalN = '0'.repeat(n.length)
-    while (n !== finalN) {
-        n = n.split('').map(str => Number(str) > 0 ? (Number(str) - 1).toString() : '0').join('');
-        count++;
-    }
-    return count;
+    return Math.max(...n.split(''));
 };
