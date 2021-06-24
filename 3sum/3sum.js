30233e14d0624b8292aafed42957826aacd9cade
William Chang <wdchang86@gmail.com>
2021-06-24 10:31:40 -0400

    Time: 136 ms (96.86%), Space: 49.4 MB (39.16%) - LeetHub

---
+++ 3sum/3sum.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    const result = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            let left = i + 1;
+            let right = nums.length - 1;
+            while (left < right) {
+                let sum = nums[i] + nums[left] + nums[right];
+                if (sum === 0) {
+                    result.push([nums[i], nums[left], nums[right]]);
+                    while (nums[left] === nums[left + 1]) left++;
+                    while (nums[right] === nums[right - 1]) right--;
+                    left++;
+                    right--;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+
+    }
+    return result;
+};
