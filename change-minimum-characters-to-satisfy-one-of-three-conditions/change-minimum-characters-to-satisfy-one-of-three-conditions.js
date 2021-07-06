72be5042c4f35ae31237c88b695097ddcdef9c12
William Chang <wdchang86@gmail.com>
2021-07-06 16:59:12 -0400

    Time: 236 ms (66.67%), Space: 50.3 MB (66.67%) - LeetHub

---
+++ change-minimum-characters-to-satisfy-one-of-three-conditions/change-minimum-characters-to-satisfy-one-of-three-conditions.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+var minCharacters = function(a, b) {
+    const arrA = new Array(26).fill(0);
+    const arrB = new Array(26).fill(0);
+    
+    for (let char of a) {
+        arrA[char.charCodeAt() - 97]++;
+    };
+    
+    for (let char of b) {
+        arrB[char.charCodeAt() - 97]++;
+    };
+
+    let min = Infinity;
+    for (let i = 0; i < 26; i++) {
+        min = Math.min(min, (a.length - arrA[i]) + (b.length - arrB[i]));
+        
+        if (i > 0) {
+            arrA[i] += arrA[i - 1];
+            arrB[i] += arrB[i - 1];
+        }
+        console.log(arrA)
+        console.log(arrB)
+        if (i < 25) {
+            min = Math.min(min, b.length - arrB[i] + arrA[i], a.length - arrA[i] + arrB[i]);
+        }
+        console.log(min)
+    }
+    return min;
+};
