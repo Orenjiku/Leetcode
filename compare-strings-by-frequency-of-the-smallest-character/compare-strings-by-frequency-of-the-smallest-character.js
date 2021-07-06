bc12601c89ffee1ab37de41e3395bb1b1b1b3079
William Chang <wdchang86@gmail.com>
2021-07-06 10:32:38 -0400

    Time: 92 ms (91.89%), Space: 43.5 MB (64.86%) - LeetHub

--- compare-strings-by-frequency-of-the-smallest-character/compare-strings-by-frequency-of-the-smallest-character.js
+++ compare-strings-by-frequency-of-the-smallest-character/compare-strings-by-frequency-of-the-smallest-character.js
@@ -6,30 +6,25 @@
 var numSmallerByFrequency = function(queries, words) {
     const queriesArr = queries.map(str => findMinFreq(str));
     const wordsArr = words.map(str => findMinFreq(str)).sort((a, b) => a - b);
-    const result = [];
-    for (let i = 0; i < queriesArr.length; i++) {
+    return queriesArr.map(queryFreq => {
         let count = 0;
         for (let j = 0; j < wordsArr.length; j++) {
-            if (queriesArr[i] < wordsArr[j]) {
+            if (queryFreq < wordsArr[j]) {
                 count = wordsArr.length - j;
                 break;
             }
         }
-        result.push(count);
-    }
-    return result;
+        return count;
+    })
 };
 
 const findMinFreq = (str) => {
     const arr = new Array(26).fill(0);
-    let max = [Infinity, -Infinity];
+    let min = Infinity;
     for (let i = 0; i < str.length; i++) {
         const charIdx = str[i].charCodeAt() - 97;
         arr[charIdx]++;
-        if (charIdx <= max[0]) {
-            max = [charIdx, arr[charIdx]];
-        }
-        
+        if (charIdx < min) min = charIdx;
     }
-    return max[1];
+    return arr[min];
 }
