5736722cc496956fccf39c71b22e7c8a243f30fa
William Chang <wdchang86@gmail.com>
2021-08-10 19:06:49 -0400

    Time: 88 ms (59.46%), Space: 42.8 MB (57.30%) - LeetHub

--- k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.js
+++ k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.js
@@ -4,18 +4,19 @@
  * @return {number}
  */
 var findPairs = function(nums, k) {
-    const set = new Set();;
-    let count = 0;
-    for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (Math.abs(nums[i] - nums[j]) === k) {
-                if (!set.has(`${nums[i]}-${nums[j]}`) || !set.has(`${nums[j]}-${nums[i]}`)) {
-                    count++;
-                    set.add(`${nums[i]}-${nums[j]}`);
-                    set.add(`${nums[j]}-${nums[i]}`);
-                }
-            }
-        }
+    const map = {};
+    for (let i = 0; i < nums.length; i++) {
+        map[nums[i]] = (map[nums[i]] || 0) + 1;
     }
+
+    let count = 0;
+    Object.keys(map).forEach(key => {
+        const num = Number(key);
+        if (k === 0 && map[num] > 1) {
+            count++;
+        } else if (k > 0 && map[num + k]) {
+            count++;
+        }
+    })
     return count;
 };
