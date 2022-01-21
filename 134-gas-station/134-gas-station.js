0d377f9e0fa3902ed233f4b326ed32b746f62569
William Chang <wdchang86@gmail.com>
2022-01-21 01:03:43 -0500

    Time: 76 ms (96.23%), Space: 46 MB (34.78%) - LeetHub

--- 134-gas-station/134-gas-station.js
+++ 134-gas-station/134-gas-station.js
@@ -4,23 +4,16 @@
  * @return {number}
  */
 var canCompleteCircuit = function(gas, cost) {
-    const gasTotal = gas.reduce((acc, cur) => acc + cur, 0);
-    const costTotal = cost.reduce((acc, cur) => acc + cur, 0);
-    if (gasTotal < costTotal) return - 1;
-    let curStation = 0;
-    let start = -1;
+    let totalGas = 0;
     let curGas = 0;
-    let count = 0;
-    while (curStation !== start) {
-        curGas += gas[curStation];
-        if (curGas < cost[curStation]) {
+    let start = 0;
+    for (let i = 0; i < gas.length; i++) {
+        curGas += gas[i] - cost[i];
+        totalGas += gas[i] - cost[i];
+        if (curGas < 0) {
             curGas = 0;
-            start = -1;
-        } else {
-            curGas -= cost[curStation];
-            if (start === -1) start = curStation;
+            start = i + 1;
         }
-        curStation = (curStation + 1) % gas.length;
     }
-    return start;
-};
+    return totalGas < 0 ? -1 : start;
+}
