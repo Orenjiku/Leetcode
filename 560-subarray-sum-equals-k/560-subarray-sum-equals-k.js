ed83028682fecd1a8e4052ee5ec23dee7db5e393
William Chang <wdchang86@gmail.com>
2022-01-21 09:39:28 -0500

    Time: 152 ms (33.71%), Space: 49.7 MB (29.14%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    let sum = 0;
+    let result = 0;
+    const map = {0: 1};
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        if (map[sum - k]) {
+            result += map[sum - k];
+        }
+        map[sum] = (map[sum] || 0) + 1;
+    }
+    return result;
+};
