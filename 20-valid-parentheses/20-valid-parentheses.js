bff94f40d99b89e97130ec3a61532200164949ae
William Chang <wdchang86@gmail.com>
2022-02-16 09:03:12 -0500

    Time: 72 ms (84.96%), Space: 42.5 MB (16.53%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -8,11 +8,10 @@ var isValid = function(s) {
     for (const c of s) {
         if (c === '(' || c === '{' || c === '[') {
             stack.push(c);
-        } else if (pairs[c] === stack[stack.length - 1]) {
-            stack.pop();
         } else {
-            return false;
+            const prevC = stack.pop();
+            if (prevC !== pairs[c]) return false;
         }
     }
-    return stack.length === 0 ? true : false
+    return stack.length === 0;
 };
