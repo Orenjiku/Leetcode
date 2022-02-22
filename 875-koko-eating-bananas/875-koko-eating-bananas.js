4162bf6daf32907be1d30b54a7a4e21be443afe7
William Chang <wdchang86@gmail.com>
2022-02-21 19:44:13 -0500

    Time: 108 ms (80.36%), Space: 45.3 MB (11.53%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.js
+++ 875-koko-eating-bananas/875-koko-eating-bananas.js
@@ -6,18 +6,16 @@
 var minEatingSpeed = function(piles, h) {
     let left = 1;
     let right = Math.max(...piles);
-    let minRate = Infinity;
-    while (left <= right) {
+    while (left < right) {
         const k = Math.floor((left + right) / 2);
         const hoursToEat = getHoursToEat(piles, k);
         if (hoursToEat > h) {
             left = k + 1;
         } else if (hoursToEat <= h) {
-            right = k - 1;
-            minRate = Math.min(minRate, k);
+            right = k;
         }
     }
-    return minRate
+    return right
 };
 
 const getHoursToEat = (piles, rate) => {
