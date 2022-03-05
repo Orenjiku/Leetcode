89fdcef75b27db80c502ee4c5ee15518c59e0da3
William Chang <wdchang86@gmail.com>
2022-03-04 22:26:31 -0500

    Time: 104 ms (68.46%), Space: 50.9 MB (16.48%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -6,12 +6,12 @@ var maxSubArray = function(nums) {
     let maxTotal = -Infinity;
     let curTotal = -Infinity;
     for (let num of nums) {
-        maxTotal = Math.max(curTotal, maxTotal);
         if (curTotal + num < num) {
             curTotal = num;
         } else {
             curTotal += num;
         }
+        maxTotal = Math.max(curTotal, maxTotal);
     }
-    return Math.max(curTotal, maxTotal);
+    return maxTotal;
 };
