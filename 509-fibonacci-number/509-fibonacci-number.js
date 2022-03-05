d242fd892c8a471c498a2ee43c2ca63710484992
William Chang <wdchang86@gmail.com>
2022-03-04 22:43:30 -0500

    Time: 1076 ms (5.01%), Space: 47.9 MB (5.08%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -3,9 +3,8 @@
  * @return {number}
  */
 var fib = function(n, memo = {}) {
-    if (memo[n]) return memo[n];
     if (n === 0) return 0
     if (n === 1) return 1;
-    memo[n] = fib(n - 1) + fib(n - 2);
+    if (!(n in memo)) memo[n] = fib(n - 1) + fib(n - 2);
     return memo[n];
 };
