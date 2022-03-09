859ce60d4995393fcda66bc0172423cefa8597c8
William Chang <wdchang86@gmail.com>
2022-03-09 15:39:37 -0500

    Time: 75 ms (94.81%), Space: 45 MB (53.65%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.js
+++ 36-valid-sudoku/36-valid-sudoku.js
@@ -3,29 +3,23 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-    const colMap = {};
-    const rowMap = {};
-	for (let row = 0; row < board.length; row += 3) {
-		for (let col = 0; col < board[row].length; col += 3) {
-			let squareSet = new Set();
-			for (let i = row; i < row + 3; i++) {
-				for (let j = col; j < col + 3; j++) {
-                    if (board[i][j] !== '.') {
-                        if (squareSet.has(board[i][j])) return false;
-                        else squareSet.add(board[i][j]);
-
-                        if (rowMap[i]?.has(board[i][j])) return false;
-                        else if (!(i in rowMap)) rowMap[i] = new Set();
-                        rowMap[i].add(board[i][j]); 
-
-                        if (colMap[j]?.has(board[i][j])) {
-                            return false;
-                        } else if (!(j in colMap)) colMap[j] = new Set();
-                        colMap[j].add(board[i][j]);
-                    }
-				}
-			}
-		}
-	}
-  return true;
+    const rowSet = new Set();
+    const colSet = new Set();
+    const gridSet = new Set();
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] !== '.') {
+                const rowKey = `${i}-${board[i][j]}`;
+                const colKey = `${j}-${board[i][j]}`;
+                const gridKey = `${Math.floor(i / 3)}-${Math.floor(j / 3)}-${board[i][j]}`;
+                if (rowSet.has(rowKey) || colSet.has(colKey) || gridSet.has(gridKey)) {
+                    return false;
+                }
+                rowSet.add(rowKey);
+                colSet.add(colKey);
+                gridSet.add(gridKey);
+            }
+        }
+    }
+    return true;
 };
