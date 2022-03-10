1c63d583ee8ac17ab0580d69d979c9e40a36ea3d
William Chang <wdchang86@gmail.com>
2022-03-10 00:21:20 -0500

    Time: 180 ms (40.26%), Space: 49.5 MB (17.05%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let map = {};
+    let maxLen = 0;
+    let curLen = 0;
+    let left = 0;
+    for (let i = 0; i < s.length; i++) {
+        map[s[i]] = (map[s[i]] || 0) + 1;
+        while (map[s[i]] > 1) {
+            map[s[left]]--;
+            curLen--;
+            left++;
+        }
+        curLen++;
+        maxLen = Math.max(maxLen, curLen);
+    }
+    return maxLen;
+};
