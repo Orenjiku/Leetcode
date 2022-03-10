314b093b79409993d46c31c46493bc3b7f30fb4b
William Chang <wdchang86@gmail.com>
2022-03-10 00:50:57 -0500

    Time: 202 ms (18.95%), Space: 49.4 MB (12.96%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -3,10 +3,11 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    const map = {};
-    for (const c of s) {
-        map[c] = (map[c] || 0) + 1;
-    }
+    const map = s.split('').reduce((acc, cur) => {
+        acc[cur] = (acc[cur] || 0) + 1;
+        return acc;
+    }, {})
+    
     for (let i = 0; i < s.length; i++) {
         if (map[s[i]] === 1) return i; 
     }
