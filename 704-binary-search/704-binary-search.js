104b395cc4519217f6f10b930ba4f1413f62100d
William Chang <wdchang86@gmail.com>
2022-03-26 11:35:07 -0400

    Time: 117 ms (14.00%), Space: 44.5 MB (93.28%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -7,14 +7,14 @@ var search = function(nums, target) {
     let left = 0;
     let right = nums.length - 1;
     while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        if (target < nums[mid]) {
-            right = mid - 1;
-        } else if (target > nums[mid]) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
             left = mid + 1;
         } else {
-            return mid;
+            right = mid - 1;
         }
     }
-    return -1
-};
+    return -1;
+}
