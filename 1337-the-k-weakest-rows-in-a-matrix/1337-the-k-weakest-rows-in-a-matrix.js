36793e60af0b84ca436d76454ceab0ad0f48da62
William Chang <wdchang86@gmail.com>
2022-03-27 20:13:12 -0400

    Time: 73 ms (79.48%), Space: 44.9 MB (27.75%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
@@ -4,18 +4,17 @@
  * @return {number[]}
  */
 var kWeakestRows = function(mat, k) {
-    const dp = [];
+    const idxArr = new Array(mat.length).fill(0).map((_, i) => i);
     const map = {};
     for (let i = 0; i < mat.length; i++) {
         let count = 0;
         for (let j = 0; j < mat[i].length; j++) {
             count += mat[i][j] === 1 ? 1 : 0;   
         }
-        dp.push(i);
         map[i] = count;
     }
     
-    return dp.sort((a, b) => {
+    return idxArr.sort((a, b) => {
         return map[a] === map[b] ? a - b : map[a] - map[b];
     }).slice(0, k);
 };
