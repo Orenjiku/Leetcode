c4494a5aeb322da9725921a41f7179e07a802f36
William Chang <wdchang86@gmail.com>
2022-04-05 12:52:41 -0400

    Time: 95 ms, Memory: 42.9 MB - LeetHub

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -4,14 +4,9 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const map = nums.reduce((acc, cur, i) => {
-        acc[cur] = i;
-        return acc;
-    }, {});
-    for (let [i, num] of nums.entries()) {
-        const num2 = target - num;
-        if (map.hasOwnProperty(num2) && map[num2] !== i) {
-            return [i, map[num2]]
-        }
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (map.hasOwnProperty(target - nums[i])) return [i, map[target - nums[i]]];
+        map[nums[i]] = i;
     }
 };
