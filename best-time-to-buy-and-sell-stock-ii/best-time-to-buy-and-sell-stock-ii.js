a13a0af7af2828903c43c47dcbbb4a4328772f66
William Chang <wdchang86@gmail.com>
2022-04-05 10:44:19 -0400

    Time: 82 ms, Memory: 42.6 MB - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -4,7 +4,6 @@
  */
 var maxProfit = function(prices) {
     let max = 0;
-    let buyPrice = Infinity;
     for (let i = 0; i < prices.length; i++) {
         if (prices[i + 1] > prices[i]) {
             max += prices[i + 1] - prices[i];
