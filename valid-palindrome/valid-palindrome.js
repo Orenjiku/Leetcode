95e46f5c9691066e1ee1f5edfbfe9e0ece811ad8
William Chang <wdchang86@gmail.com>
2022-04-05 15:50:14 -0400

    Time: 76 ms, Memory: 46.6 MB - LeetHub

---
+++ valid-palindrome/valid-palindrome.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.replace(/[^A-Za-z0-9]/g, '');
+    return s.toLowerCase() === s.split('').reverse().join('').toLowerCase();
+};
