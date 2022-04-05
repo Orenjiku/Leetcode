3266e578f1d158624eb7805c5d544389293eb64f
William Chang <wdchang86@gmail.com>
2022-04-05 11:54:51 -0400

    Time: 105 ms, Memory: 42.2 MB - LeetHub

---
+++ plus-one/plus-one.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let i = digits.length - 1;
+    while (i >= 0) {
+        const newDigit = digits[i] + 1;
+        if (newDigit < 10) {
+            digits[i] = newDigit;
+            break;
+        } else {
+            digits[i] = 0;
+            i--;
+            if (i === -1) {
+                digits.unshift(1);
+            }
+        }
+    }
+    return digits;
+};
