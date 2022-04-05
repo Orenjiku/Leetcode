fde64e2d066782553785f11e64d9e806d7573b69
William Chang <wdchang86@gmail.com>
2022-04-05 13:43:21 -0400

    Time: 119 ms, Memory: 47.7 MB - LeetHub

--- valid-sudoku/valid-sudoku.js
+++ valid-sudoku/valid-sudoku.js
@@ -5,12 +5,12 @@
 var isValidSudoku = function(board) {
     const map = {};
     for (let i = 0; i < board.length; i++) {
+        const rowsKey = `rows${i}`;
+        map[rowsKey] = new Set();
         for (let j = 0; j < board[i].length; j++) {
             if (board[i][j] === '.') continue;
-            const rowsKey = `rows${i}`;
             const colsKey = `cols${j}`;
             const diagsKey = `diags${Math.floor(i / 3)}-${Math.floor(j / 3)}`
-            if (!map.hasOwnProperty(rowsKey)) map[rowsKey] = new Set();
             if (!map.hasOwnProperty(colsKey)) map[colsKey] = new Set();
             if (!map.hasOwnProperty(diagsKey)) map[diagsKey] = new Set();
             if (map[rowsKey].has(board[i][j]) || 
