c78cece9c6fa28a36b7c0a144a709afdebf0d572
William Chang <wdchang86@gmail.com>
2022-04-05 12:40:07 -0400

    Time: 80 ms, Memory: 47.2 MB - LeetHub

--- move-zeroes/move-zeroes.js
+++ move-zeroes/move-zeroes.js
@@ -3,21 +3,12 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let left = 0;
-    let right = 0;
-    while (left < nums.length && right < nums.length) {
-        if (nums[left] !== 0 && nums[right] !== 0) {
-            left++;
-            right++;
-        } else {
-            [nums[left], nums[right]] = [nums[right], nums[left]];
-            while (nums[right] === 0) {
-                right++;
-            }
-            while (nums[left] !== 0 && nums[left] !== undefined) {
-                left++;
-            }    
+    let index = 0;
+    for (let i=0; i<nums.length; i++) {
+        if (nums[i] !== 0) {
+            [nums[index], nums[i]] = [nums[i], nums[index]]
+            index++;
         }
     }
-    return nums;
+    return nums
 };
