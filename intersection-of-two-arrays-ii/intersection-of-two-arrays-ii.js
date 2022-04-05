43eeb709e2ba845d81a953c97f7a913ec03415b0
William Chang <wdchang86@gmail.com>
2022-04-05 11:42:58 -0400

    Time: 147 ms, Memory: 42.9 MB - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
@@ -4,21 +4,25 @@
  * @return {number[]}
  */
 var intersect = function(nums1, nums2) {
-    if (nums2.length > nums1.length) return intersect(nums2, nums1);
+    if (nums1.length > nums2.length) return intersect(nums2, nums1);
     nums1.sort((a, b) => a - b);
     nums2.sort((a, b) => a - b);
-    const result = [];
-    let i = 0;
-    let j = 0;
-    while (j < nums2.length && i < nums1.length) {
-        if (nums1[i] === nums2[j]) {
-            result.push(nums1[i]);
-            i++;
-            j++;
-        } else if (nums1[i] > nums2[j]) {
-            j++;
-        } else if (nums1[i] < nums2[j]) {
-            i++;
+    let p1 = 0;
+    let p2 = 0;
+    let result = [];
+    while (p1 < nums1.length && p2 < nums2.length) {
+        if (nums1[p1] === nums2[p2]) {
+            result.push(nums1[p1]);
+            p1++;
+            p2++;
+        } else if (nums1[p1] < nums2[p2]) {
+            while (nums1[p1] < nums2[p2]) {
+                p1++;
+            }
+        } else if (nums1[p1] > nums2[p2]) {
+            while (nums1[p1] > nums2[p2]) {
+                p2++;
+            }
         }
     }
     return result;
