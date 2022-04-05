4288a55d2f2c2371741e686c1a6c41f6e54eb1f1
William Chang <wdchang86@gmail.com>
2022-04-05 11:43:48 -0400

    Time: 110 ms, Memory: 43.4 MB - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
@@ -15,14 +15,12 @@ var intersect = function(nums1, nums2) {
             result.push(nums1[p1]);
             p1++;
             p2++;
-        } else if (nums1[p1] < nums2[p2]) {
-            while (nums1[p1] < nums2[p2]) {
-                p1++;
-            }
-        } else if (nums1[p1] > nums2[p2]) {
-            while (nums1[p1] > nums2[p2]) {
-                p2++;
-            }
+        }
+        while (nums1[p1] < nums2[p2]) {
+            p1++;
+        }
+        while (nums1[p1] > nums2[p2]) {
+            p2++;
         }
     }
     return result;
