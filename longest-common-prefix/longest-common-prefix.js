c3532e5962ab8d58679f23c9cf6f1d011539b05e
William Chang <wdchang86@gmail.com>
2022-04-05 17:07:13 -0400

    Time: 87 ms, Memory: 42.6 MB - LeetHub

--- longest-common-prefix/longest-common-prefix.js
+++ longest-common-prefix/longest-common-prefix.js
@@ -3,12 +3,39 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    if (strs.length === 0) return '';
-    for (let i = 1; i <= strs[0].length; i++) {
-        const pattern = strs[0].slice(0, i)
-        for (let j = 1; j < strs.length; j++) {
-            if (pattern !== strs[j].slice(0, i)) return strs[j].slice(0, i - 1)
+    const trie = new Trie();
+    trie.build(strs);
+
+    let longestPrefix = '';
+    let curTrie = trie;
+    while (Object.keys(curTrie.children).length === 1 && curTrie.end === false) {
+        const letter = Object.keys(curTrie.children)[0];
+        longestPrefix += letter;
+        curTrie = curTrie.children[letter]
+    }
+    return longestPrefix;
+};
+
+class Trie {
+    constructor(value) {
+        this.value = '';
+        this.children = {};
+        this.end = false;
+    }
+    
+    build(array) {
+        for (let i = 0; i < array.length; i++) {
+            let curTrie = this;
+            if (array[i] === '') {
+                curTrie.children[''] = new Trie('');
+            } else {
+                for (let j = 0; j < array[i].length; j++) {
+                    const letter = array[i][j];
+                    if (!curTrie.children[letter]) curTrie.children[letter] = new Trie(letter);
+                    curTrie = curTrie.children[letter];
+                }    
+            }
+            curTrie.end = true;
         }
     }
-    return strs[0];
-};
+}
