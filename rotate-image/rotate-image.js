9d8eb3754b1727f22e2b15aedec020404cdc7ff5
William Chang <wdchang86@gmail.com>
2022-04-05 14:39:33 -0400

    Time: 82 ms, Memory: 41.8 MB - LeetHub

--- rotate-image/rotate-image.js
+++ rotate-image/rotate-image.js
@@ -3,13 +3,30 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < i; j++) {
-            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    let top = 0;
+    let left = 0;
+    let right = matrix[0].length - 1;
+    let bottom = matrix.length - 1;
+    while (top < bottom && left < right) {
+        let topLeft = [top, left];
+        let topRight = [top, right];
+        let botLeft = [bottom, left];
+        let botRight = [bottom, right];
+        while (topLeft[1] < right) {
+            let temp = matrix[topLeft[0]][topLeft[1]];
+            matrix[topLeft[0]][topLeft[1]] = matrix[botLeft[0]][botLeft[1]];
+            matrix[botLeft[0]][botLeft[1]] = matrix[botRight[0]][botRight[1]];
+            matrix[botRight[0]][botRight[1]] = matrix[topRight[0]][topRight[1]];
+            matrix[topRight[0]][topRight[1]] = temp;
+            topLeft[1]++;
+            topRight[0]++;
+            botRight[1]--;
+            botLeft[0]--;
         }
-    }
-    for (let i = 0; i < matrix.length; i++) {
-        matrix[i].reverse();
+        top++;
+        left++;
+        right--;
+        bottom--;
     }
     return matrix;
 };
