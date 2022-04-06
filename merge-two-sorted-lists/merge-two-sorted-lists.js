c143ee3ad474516659b15cddfd9b1819d95983b2
William Chang <wdchang86@gmail.com>
2022-04-06 11:20:35 -0400

    Time: 103 ms, Memory: 44.3 MB - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.js
+++ merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -11,20 +11,20 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    if (!l1 || !l2) return l1 || l2;
-    if (l1?.val > l2?.val) return mergeTwoLists(l2, l1);
-    const head = l1;
+    if (!l1) return l2;
+    if (!l2) return l1;
+    if (l1 && l2 && l1.val > l2.val) return mergeTwoLists(l2, l1);
+    let head = l1;
     while (l1 !== null && l2 !== null) {
-        if (l1.next === null) {
-            l1.next = l2;
-            break;
-        }
-        if (l1.next.val <= l2.val) {
+        if (l1.next && l1.next.val < l2.val) {
             l1 = l1.next;
         } else {
-            const temp = l1.next;
+            const next1 = l1.next;
+            const next2 = l2.next;
             l1.next = l2;
-            l2 = temp;
+            l2.next = next1;
+            l1 = l2;
+            l2 = next2;
         }
     }
     return head;
