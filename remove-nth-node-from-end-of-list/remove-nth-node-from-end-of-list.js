44eed47f3c4481fafbfa9373cebdb4a71acc05b5
William Chang <wdchang86@gmail.com>
2022-04-06 10:20:40 -0400

    Time: 99 ms, Memory: 42.2 MB - LeetHub

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
@@ -15,19 +15,16 @@ var removeNthFromEnd = function(head, n) {
     let left = head;
     let right = head;
     while (right !== null) {
-        right = right.next;
         prevLeft = left;
         if (n <= 0) left = left.next;
+        right = right.next;
         n--;
     }
     
     if (left === head) {
         return head.next;
-    } else if (left.next !== null) {
-        prevLeft.next = left.next;
-        return head;
-    } else if (!left.next) {
-        prevLeft.next = null;
-        return head;
+    } else {
+        prevLeft.next = left.next !== null ? left.next : null;
+        return head;   
     }
 };
