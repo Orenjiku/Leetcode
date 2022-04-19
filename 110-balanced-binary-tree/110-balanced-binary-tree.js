a6b41f548591f197c48b45a9fa6cfef4ad82183b
William Chang <wdchang86@gmail.com>
2022-04-19 17:57:08 -0400

    Time: 106 ms (45.28%), Space: 47.7 MB (29.13%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    return traverse(root).isBalanced;
+};
+
+const traverse = (node) => {
+    if (node === null) return {length: 0, isBalanced: true};
+    const left = traverse(node.left);
+    const right = traverse(node.right);
+    const length = Math.max(left.length, right.length) + 1;
+    const isBalanced = left.isBalanced && right.isBalanced && Math.abs(left.length - right.length) <= 1;
+    return {length, isBalanced}
+}
