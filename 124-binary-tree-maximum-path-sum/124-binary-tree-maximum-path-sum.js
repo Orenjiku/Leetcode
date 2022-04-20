f38677352ad4188da9bbf538fe31dd02f55162c6
William Chang <wdchang86@gmail.com>
2022-04-20 17:47:35 -0400

    Time: 94 ms (69.42%), Space: 52.5 MB (6.80%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.js
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.js
@@ -13,11 +13,12 @@
 var maxPathSum = function(root) {
 	const postOrderTraverse = (node) => {
 		if (!node) return [0, -Infinity];
-		let left = postOrderTraverse(node.left);
-		let right = postOrderTraverse(node.right);
-        let maxLeft = Math.max(0, left[0]);
-        let maxRight = Math.max(0, right[0]);
-		maxSum = Math.max(Math.max(left[1], right[1]), node.val + maxLeft + maxRight);
+		const left = postOrderTraverse(node.left);
+		const right = postOrderTraverse(node.right);
+        const maxLeft = Math.max(0, left[0]);
+        const maxRight = Math.max(0, right[0]);
+        const prevMaxSum = Math.max(left[1], right[1]); 
+		const maxSum = Math.max(prevMaxSum, node.val + maxLeft + maxRight);
 		return [node.val + Math.max(maxLeft, maxRight), maxSum];
 	}
 	return postOrderTraverse(root)[1];
