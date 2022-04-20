2c3e682e094bed00f19c7f80a25b9539c6b2fe94
William Chang <wdchang86@gmail.com>
2022-04-19 20:07:46 -0400

    Time: 142 ms (16.06%), Space: 51.6 MB (97.92%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,16 +13,16 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    return traverse(root, p, q)[1];  
+    return traverse(root, p, q).LCA;
 };
 
 const traverse = (root, p, q) => {
-    if (root === null) return [0, null]
+    if (root === null) return {count: 0, LCA: null}
     const left = traverse(root.left, p, q);
     const right = traverse(root.right, p, q);
 
-    let count = left[0] + right[0];
+    let count = left.count + right.count;
     if (root === p || root === q) count++;
-    if (count === 2 && left[1] === null && right[1] === null ) return [count, root];
-    return [count, left[1] || right[1]]
+    if (count === 2 && left.LCA === null && right.LCA === null ) return {count, LCA: root};
+    return {count, LCA: left.LCA || right.LCA}
 }
