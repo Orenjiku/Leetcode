5024684f404c40e9922edb4e7e2b349a26bfebf3
William Chang <wdchang86@gmail.com>
2022-04-21 09:02:14 -0400

    Time: 103 ms (57.62%), Space: 42.1 MB (89.59%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -6,7 +6,11 @@
 var twoSum = function(nums, target) {
     const map = {};
     for (let i = 0; i < nums.length; i++) {
-        if (map.hasOwnProperty(target - nums[i])) return [map[target - nums[i]], i];
-        else map[nums[i]] = i;
+        if (map.hasOwnProperty(target - nums[i])) {
+            return [map[target - nums[i]], i]
+        } else {
+            map[nums[i]] = i;
+        }
     }
+    return -1;
 };
