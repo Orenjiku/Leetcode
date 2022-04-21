ce35996e86cfe8b9f88c7d373b8151324ee4178f
William Chang <wdchang86@gmail.com>
2022-04-21 09:50:37 -0400

    Time: 350 ms (7.87%), Space: 58.8 MB (6.85%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -3,15 +3,11 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    const sortedStrs = [...strs];
     const map = {}
-    for (let i = 0; i < sortedStrs.length; i++) {
-        const key = sortedStrs[i].split('').sort((a, b) => a.localeCompare(b)).join('');
-        if (map.hasOwnProperty(key)) {
-            map[key].push(strs[i]);
-        } else {
-            map[key] = [strs[i]];
-        }
+    for (let i = 0; i < strs.length; i++) {
+        const key = strs[i].split('').sort((a, b) => a.localeCompare(b)).join('');
+        if (!map.hasOwnProperty(key)) map[key] = []
+        map[key].push(strs[i]);
     }
     return Object.values(map);
 };
