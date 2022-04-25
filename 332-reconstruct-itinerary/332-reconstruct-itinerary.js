794dd85aace75602e9bb5f3c1f25692ba55affbc
William Chang <wdchang86@gmail.com>
2022-04-25 13:12:07 -0400

    Time: 148 ms (27.56%), Space: 47.2 MB (67.84%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.js
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.js
@@ -3,24 +3,24 @@
  * @return {string[]}
  */
 var findItinerary = function(tickets) {
-   let graph = {}
-   let res = []
+   let graph = {};
+   let res = [];
    for (let [dep, des] of tickets) {
-       if (!graph[dep]) graph[dep] = []
-       graph[dep].push(des)
+       if (!graph[dep]) graph[dep] = [];
+       graph[dep].push(des);
    }
     
     for (let dep in graph) {
-        graph[dep].sort().reverse()
+        graph[dep].sort();
     }
 
     let dfs = (node) =>{
         let des = graph[node]
         while(des && des.length > 0) {
-            dfs(des.pop())
+            dfs(des.shift());
         }
-        res.push(node)
+        res.unshift(node);
     }
-    dfs("JFK")
-    return res.reverse()
+    dfs("JFK");
+    return res;
 };
