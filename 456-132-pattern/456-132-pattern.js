b3e8b4c869ce230016bf629eb5315686f30e57b3
William Chang <wdchang86@gmail.com>
2022-05-09 17:18:29 -0400

    Time: 119 ms (39.39%), Space: 53.1 MB (59.09%) - LeetHub

--- 456-132-pattern/456-132-pattern.js
+++ 456-132-pattern/456-132-pattern.js
@@ -7,7 +7,7 @@ var find132pattern = function(nums) {
     let s3 = -Infinity
     for (let i = nums.length - 1; i >= 0; i--) {
         if (nums[i] < s3) return true
-        while (stack.length > 0 && stack[stack.length - 1] < nums[i]) {
+        while (stack.length > 0 && nums[i] > stack[stack.length - 1]) {
             s3 = stack.pop()
         }
         stack.push(nums[i])
