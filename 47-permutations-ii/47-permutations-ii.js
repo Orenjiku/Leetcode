9cf88dad41831ae47916e6bcbd4c93256bf1ab40
William Chang <wdchang86@gmail.com>
2022-05-12 10:32:47 -0400

    Time: 223 ms (21.83%), Space: 48.9 MB (27.98%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.js
+++ 47-permutations-ii/47-permutations-ii.js
@@ -4,23 +4,22 @@
  */
 var permuteUnique = function(nums) {
     const result = [];
-    const combo = new Set();
-    dfs(nums, [], combo, result);
+    const comboSet = new Set();
+    dfs(nums, [], '', comboSet, result);
     return result;
 };
 
-const dfs = (arr, curCombo, combo, result) => {
+const dfs = (arr, comboArr, comboStr, comboSet, result) => {
     if (arr.length === 0) {
-        const comboStr = curCombo.join('')
-        if (!combo.has(comboStr)) {
-            combo.add(comboStr);
-            result.push([...curCombo]);
+        if (!comboSet.has(comboStr)) {
+            comboSet.add(comboStr);
+            result.push([...comboArr]);
         }
         return;
     }
     for (let i = 0; i < arr.length; i++) {
-        curCombo.push(arr[i]);
-        dfs([...arr.slice(0, i), ...arr.slice(i + 1)], curCombo, combo, result);
-        curCombo.pop();
+        comboArr.push(arr[i]);
+        dfs([...arr.slice(0, i), ...arr.slice(i + 1)], comboArr, comboStr + arr[i], comboSet, result);
+        comboArr.pop();
     }
 }
