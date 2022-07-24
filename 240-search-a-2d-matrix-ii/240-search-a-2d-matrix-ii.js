cfb55f3a5f155fbf008fe56f5586d16a5a4d09c1
William Chang <wdchang86@gmail.com>
2022-07-23 20:29:04 -0400

    Time: 677 ms (60.69%), Space: 45.2 MB (45.79%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.js
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.js
@@ -6,7 +6,7 @@
 var searchMatrix = function(matrix, target) {
     let row = 0;
     let col = matrix[0].length - 1;
-    while (row >= 0 && row < matrix.length && col >= 0 && col < matrix[0].length) {
+    while (row < matrix.length && col >= 0) {
         if (target === matrix[row][col]) return true;
         if (target < matrix[row][col]) {
             col--;
