2cc46dd014cbcb641be49b1b6d4019d0c98675e3
William Chang <wdchang86@gmail.com>
2022-07-23 21:13:16 -0400

    Time: 90 ms (56.52%), Space: 43.9 MB (52.17%) - LeetHub

---
+++ 86-partition-list/86-partition-list.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function(head, x) {
+    const lowStartNode = new ListNode(0);
+    const highStartNode = new ListNode(0);
+    let lowNode = lowStartNode;
+    let highNode = highStartNode;
+    while (head !== null) {
+        if (head.val < x) {
+            lowNode.next = head;
+            lowNode = lowNode.next;
+        } else {
+            highNode.next = head;
+            highNode = highNode.next;
+        }
+        head = head.next;
+    }
+    highNode.next = null;
+    lowNode.next = highStartNode.next;
+    return lowStartNode.next;
+};
