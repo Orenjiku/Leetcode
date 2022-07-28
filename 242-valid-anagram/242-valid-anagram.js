d1332ad23404855351d837b8c69d3ee4c95f4582
William Chang <wdchang86@gmail.com>
2022-07-28 09:44:11 -0400

    Time: 221 ms (6.49%), Space: 48.2 MB (19.05%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -5,12 +5,5 @@
  */
 
 var isAnagram = function(s, t) {
-    if (s.length !== t.length) return false;
-    s = s.split('').sort((a, b) => a.charCodeAt() - b.charCodeAt());
-    t = t.split('').sort((a, b) => a.charCodeAt() - b.charCodeAt());
-
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] !== t[i]) return false;
-    }
-    return true;
+    return s.split('').sort((a, b) => a.charCodeAt() - b.charCodeAt()).join('') === t.split('').sort((a, b) => a.charCodeAt() - b.charCodeAt()).join('');
 };
