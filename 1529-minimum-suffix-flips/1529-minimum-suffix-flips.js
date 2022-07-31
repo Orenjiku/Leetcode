940ad06ddc3aa3ef766fc0de242a28b4c73f0669
William Chang <wdchang86@gmail.com>
2022-07-31 15:03:06 -0400

    Time: 122 ms (26.53%), Space: 43.4 MB (79.59%) - LeetHub

--- 1529-minimum-suffix-flips/1529-minimum-suffix-flips.js
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.js
@@ -4,11 +4,11 @@
  */
 var minFlips = function(target) {
   let ans = 0
-  let cur = 0;
+  let flip = 0;
   for (let i = 0; i < target.length; i++) {
-    if ((target[i] === '1' && cur === 0) || (target[i] === '0' && cur === 1)) {
+    if ((target[i] === '1' && flip === 0) || (target[i] === '0' && flip === 1)) {
       ans += 1;
-      cur = cur === 0 ? 1 : 0;
+      flip = flip === 0 ? 1 : 0;
     }
   }
   return ans;
