cafb6231c892525e9a6f02592f788ffcefb1e1f4
William Chang <wdchang86@gmail.com>
2022-08-06 15:07:32 -0400

    Time: 134 ms (24.51%), Space: 45.4 MB (33.36%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} isConnected
+ * @return {number}
+ */
+var findCircleNum = function(isConnected) {
+    const parent = Array.from(new Array(isConnected.length), (_, i) => i);
+    const rank = parent.map(v => 0);
+
+    for (let i = 0; i < isConnected.length; i++) {
+        for (let j = 0; j < isConnected[i].length; j++) {
+            if (isConnected[i][j] === 1) union(i, j, parent, rank);
+        }
+    }
+    return parent.filter((v, i) => v === i).length;
+};
+
+const union = (c1, c2, parent, rank) => {
+    const p1 = find(c1, parent);
+    const p2 = find(c2, parent);
+    if (p1 === p2) return;
+    if (rank[p1] < rank[p2]) {
+        parent[p1] = p2;
+    } else if (rank[p2] < rank[p1]) {
+        parent[p2] = p1;
+    } else {
+        parent[p1] = p2;
+        rank[p2]++;
+    }
+}
+
+const find = (c, parent) => {
+    if (parent[c] !== c) parent[c] = find(parent[c], parent);
+    return parent[c];
+}
