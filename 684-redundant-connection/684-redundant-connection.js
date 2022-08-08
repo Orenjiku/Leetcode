cb3f1fc7e23efcb1325b30a7ff0ab3fdaa25dbbb
William Chang <wdchang86@gmail.com>
2022-08-08 13:49:43 -0400

    Time: 157 ms (11.65%), Space: 46 MB (29.65%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.js
+++ 684-redundant-connection/684-redundant-connection.js
@@ -3,31 +3,31 @@
  * @return {number[]}
  */
 var findRedundantConnection = function(edges) {
-    const parent = Array.from(new Array(edges.length + 1), (_, i) => i);
-    const rank = new Array(edges.length + 1).fill(0);
+    const adjMap = {};
+    let redundantEdge = null;
+    
     for (const [u, v] of edges) {
-        if (union(u, v, parent, rank)) return [u, v]
+        const visited = new Set();
+        if (dfs(u, v, visited, adjMap)) redundantEdge = [u, v];
+        
+        if (!(u in adjMap)) adjMap[u] = new Set();
+        if (!(v in adjMap)) adjMap[v] = new Set();
+        
+        adjMap[u].add(v);
+        adjMap[v].add(u);
     }
-    return [];
+    
+    return redundantEdge;
 };
 
-const union = (c1, c2, parent, rank) => {
-    const p1 = find(c1, parent);
-    const p2 = find(c2, parent);
-    if (p1 === p2) return true;
-    
-    if (rank[p1] < rank[p2]) {
-        parent[p1] = p2;
-    } else if (rank[p2] < rank[p1]) {
-        parent[p2] = p1;
-    } else {
-        parent[p1] = p2;
-        rank[p2]++;
+const dfs = (u, v, visited, adjMap) => {
+    if (!(u in adjMap)) return false;
+    if (visited.has(v)) return true;
+    visited.add(u);
+    for (const w of adjMap[u]) {
+        if (visited.has(w)) continue
+        visited.add(w);
+        if (dfs(w, v, visited, adjMap)) return true;
     }
     return false;
-}
-
-const find = (c, parent) => {
-    if (parent[c] !== c) parent[c] = find(parent[c], parent);
-    return parent[c];
 }
